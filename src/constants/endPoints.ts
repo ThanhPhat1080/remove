33547c82e441e500364abaa56a9732aa9bc69d9c
Phat Truong <thanhphat.994@gmail.com>
2022-11-17 06:10:27 +0000

    Update 3 files

--- src/constants/endPoints.ts
+++ src/constants/endPoints.ts
@@ -1,4 +1,5 @@
-const ENDPOINT = import.meta.env.VITE_API_URL;
+//const ENDPOINT = import.meta.env.VITE_API_URL;
+const ENDPOINT = 'https://j807mv-3001.preview.csb.app';
 
 export const USER_ENDPOINT = ENDPOINT + '/users';
 export const TODO_ENDPOINT = ENDPOINT + '/todos';
